f8392ca408d2be87cd120115a4d0e37c5962fc46
gabrielxavii <mdepression00@gmail.com>
2026-07-07 23:45:47 -0300

    Implementação de edição de transportadoras

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from database import(
+    criar_banco,
+    cadastrar_transportadora,
+    listar_transportadoras,
+    atualizar_transportadora,
+    buscar_transportadora
+)
 
 app = Flask(__name__)
+
+criar_banco()
 
 @app.route("/")
 def dashboard():
@@ -14,9 +23,41 @@ def leitura():
 def roteiros():
     return render_template("roteiros.html")
 
-@app.route("/transportadoras")
+@app.route("/transportadoras", methods=["GET", "POST"])
 def transportadoras():
-    return render_template("transportadora.html")
+
+    if request.method == "POST":
+
+        nome = request.form["nome"]
+
+        cadastrar_transportadora(nome)
+
+        return redirect("/transportadoras")
+    
+    transportadoras = listar_transportadoras()
+
+    return render_template(
+        "transportadoras.html",
+        transportadoras=transportadoras
+    )
+
+@app.route("/transportadoras/<int:id>/editar", methods=["GET", "POST"])
+def editar_transportadoras(id):
+
+    if request.method == "POST":
+
+        nome = request.form["nome"]
+
+        atualizar_transportadora(id, nome)
+
+        return redirect("/transportadoras")
+    
+    transportadora = buscar_transportadora(id)
+
+    return render_template(
+            "editar_transportadora.html",
+            transportadora=transportadora
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,28 +1,38 @@
 import sqlite3
+# ==========================
+# CONEXÃO
+# ==========================
+def conectar():
+    return sqlite3.connect("database.db")
 
-conexao = sqlite3.connect("database.db")
-cursor = conexao.cursor()
+# ==========================
+# CRIAÇÃO DO BANCO
+# ==========================
+def criar_banco():
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS transportadoras (
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transportadoras (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     
     nome TEXT NOT NULL UNIQUE
-)
-""")
+    )
+    """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS roteiros(
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS roteiros(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     
     data TEXT NOT NULL UNIQUE,
                
     status TEXT NOT NULL
-)
-""")
+    )
+    """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS notas (
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS notas (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
         
     numero_nf TEXT NOT NULL UNIQUE,
@@ -40,8 +50,92 @@ CREATE TABLE IF NOT EXISTS notas (
     
     FOREIGN KEY (roteiro_id)
         REFERENCES roteiros(id)
-)
-""")
+    )
+    """)
 
-conexao.commit()
-conexao.close()
+    conexao.commit()
+    conexao.close()
+
+# ==========================
+# TRANSPORTADORAS
+# ==========================
+
+def cadastrar_transportadora(nome):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        INSERT INTO transportadoras (nome)
+        VALUES(?)
+    """, (nome,))
+
+    conexao.commit()
+    conexao.close()
+
+def listar_transportadoras():
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        SELECT * FROM transportadoras
+        ORDER BY nome
+    """)
+
+    transportadoras = cursor.fetchall()
+
+    conexao.close()
+
+    return transportadoras
+
+def buscar_transportadora(id):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        SELECT * FROM transportadoras
+        WHERE id = ?
+    """, (id,))
+
+    transportadora = cursor.fetchone()
+
+    conexao.close()
+
+    return transportadora
+
+def atualizar_transportadora(id, nome):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        UPDATE transportadoras
+        SET nome = ?
+        WHERE id = ?
+    """, (
+        nome,
+        id
+    ))
+
+    conexao.commit()
+    conexao.close()
+
+def excluir_transportadora(id):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        DELETE FROM transportadoras
+        WHERE id = ?
+    """, (id,))
+
+    conexao.commit()
+    conexao.close()
+
+
+
+if __name__ == "__main__":
+    criar_banco()
